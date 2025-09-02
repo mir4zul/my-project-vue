4310cdda1008739ed1f75bf3e342ad21b176efb7
mir4zul <mirajulislam0509@gmail.com>
2025-09-03 00:38:21 +0600

    add routes for vue route

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,20 @@
+<script setup>
+import {ref} from "vue";
+
+let message = ref('About page')
+
+setTimeout(() => {
+  message.value = 'About page updated'
+}, 2000)
+
+</script>
+
 <template>
   <div>
     <RouterLink to="/">Home</RouterLink>
     <h1>About</h1>
+    {{ message }}
+
+    <input v-model="message" class="border border-gray-300 rounded" type="text">
   </div>
 </template>
