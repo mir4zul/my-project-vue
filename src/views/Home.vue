ee90a2501a57544d23c3ec616d22273057ba7c34
mir4zul <mirajulislam0509@gmail.com>
2025-09-05 00:04:33 +0600

    Create localStorage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,23 @@
+<script setup>
+import {ref} from "vue";
+
+const name = ref(localStorage.getItem("name") || "");
+
+function write() {
+  localStorage.setItem("name", name.value);
+}
+
+</script>
+
 <template>
   <div class="flex flex-col items-center justify-center">
     <RouterLink to="/about">About</RouterLink>
     <h1 class="text-2xl font-bold">Home</h1>
+
+    <div class="">
+      <input v-model="name" class="border border-gray-300 rounded p-2" placeholder="Enter your name" type="text"
+             @input="write"/>
+      <p>{{ name }}</p>
+    </div>
   </div>
 </template>
